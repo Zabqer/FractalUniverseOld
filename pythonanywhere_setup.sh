23950218b56494b89ed10db62d797271f10f9bbf
Zabqer <zabqer@gmail.com>
2019-03-16 13:56:20 +0300

    Fix install script

--- pythonanywhere_setup.sh
+++ pythonanywhere_setup.sh
@@ -2,27 +2,29 @@
 
 echo "[pythonanywhere.com installing script]"
 
-make_virtual_env() {
-  echo "Making virtual environment"
-  python3 -m venv VirtualEnvironment
-  source VirtualEnvironment/bin/activate
-  pip install -r requirements.txt
-  deactivate
-}
-
 clone_repo() {
   echo "Cloning repository"
   git clone https://github.com/Zabqer/FractalUniverse.git
 }
 
+make_virtual_env() {
+  echo "Making virtual environment"
+  python3 -m venv VirtualEnvironment
+  source VirtualEnvironment/bin/activate
+  pip install -r FractalUniverse/Backend/requirements.txt
+  python3 FractalUniverse/Backend/manage.py makemigrations
+  python3 FractalUniverse/Backend/manage.py migrate
+  deactivate
+}
+
 set_wsgi_config() {
   wsgi_file="/var/www/${USER,,}_pythonanywhere_com_wsgi.py"
   echo "Setting WSGI file. Location: $wsgi_file"
-  cat Backend/FractalUniverse/FractalUniverse/wsgi.py > $wsgi_file
+  cat FractalUniverse/Backend/FractalUniverse/FractalUniverse/wsgi.py > $wsgi_file
 }
 
-make_virtual_env
 clone_repo
+make_virtual_env
 set_wsgi_config
 
 echo "My job is done"
